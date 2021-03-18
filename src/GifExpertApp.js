1889c43c0f5a31e58eb8f7fb3ef3127e53592f05
Alex Maldonado <alex.maldonado@outlook.com>
2021-03-17 22:08:11 -0300

    Add new component 'AddCategory.js'

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import AddCategory from './components/AddCategory';
 
 const GifExpertApp = () => {
 
+
+    const [categories, setCategories] = useState(['Breaking Bad', 'Mr. Robot', 'WandaVision']);
+
+    // const handleAdd = () => {
+    //     setCategories([...categories,'Better Call Saul']);
+    // };
+
     return (
         <>
+
         <h2>GifExpertApp</h2>
+        <AddCategory setCategories={setCategories} />
         <hr/>
+
+        <ul>
+
+            {
+                categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })
+            }
+
+        </ul>
         </>
     );
 
